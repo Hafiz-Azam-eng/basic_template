40a294ab23ad47e9032ec513487879ce6a3597a7
Hafiz-Azam-eng <ranaazamdilawar@gmail.com>
2025-03-15 03:07:05 +0500

    Integrated pundit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  include Pundit::Authorization
   allow_browser versions: :modern
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 end
